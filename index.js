52386673c48999abb219f32653510fe96b7ea58d
AshleyEvonne <ashleyevonne345@gmail.com>
2024-06-14 13:01:44 -0400

    Added an JS file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
   try{
-      const centralDB = await central(id);
-      const userInfo = await (centralDB === "db1" ? db1(id): centralDB === "db2" ? db2(id): centralDB === "db3" ? db3(id))
+      const centralDB = await central(id); // used to access the data of each data base
+      const userInfo = await dbs[centralDB](id);
       const personalData = await vault(id);
 
       return {
@@ -27,6 +27,8 @@ function getUserData(id) {
     return Promise.reject(err.message);
   }
 }
+
+
 getUserData(2)
 .then(data => console.log(data))
 .catch(error => console.error(error))
